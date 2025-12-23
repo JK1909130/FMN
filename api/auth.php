d3c2b0c9a2925b20d6b685571b93c5347c892135
JK1909130 <khuong19092005@gmail.com>
2025-12-23 16:38:25 +0800

    Update auth.php

--- api/auth.php
+++ api/auth.php
@@ -2,7 +2,6 @@
 session_start();
 
 if (!isset($_SESSION["user_id"])) {
-    http_response_code(401);
-    echo json_encode(["error" => "Unauthorized"]);
+    header("Location: /login.html");
     exit;
 }
