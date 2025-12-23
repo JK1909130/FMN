57485ab2c04875c01a4bb2f718601059dc32bf72
JK1909130 <khuong19092005@gmail.com>
2025-12-23 13:05:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$file = __DIR__ . $path;
+
+if ($path !== "/" && file_exists($file)) {
+    require $file;
+    exit;
+}
+
+http_response_code(404);
+echo "Not Found";
