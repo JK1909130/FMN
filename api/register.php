9ff25b8ffc7c8d38a8750e0905121deda0ae2ad6
JK1909130 <khuong19092005@gmail.com>
2025-12-23 14:19:28 +0800

    Update register.php

--- api/register.php
+++ api/register.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "db.php";
 header("Content-Type: application/json");
 
@@ -38,3 +42,4 @@ try {
     http_response_code(409);
     echo json_encode(["error" => "User already exists"]);
 }
+
